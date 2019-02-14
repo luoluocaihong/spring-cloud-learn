ef61a2d76af8f40e356fba3019a6c8f8ded7c087
xuhong <xu.hong20@iwhalecloud.com>
2019-02-14 13:30:31 +0800

    应用程序关闭时自动注销微服务实例

--- sc-learn-service-deregister/src/main/java/com/luoluocaihong/springcloud/learn/servicederegister/ServiceApplication.java
+++ sc-learn-service-deregister/src/main/java/com/luoluocaihong/springcloud/learn/servicederegister/ServiceApplication.java
@@ -1,8 +1,12 @@
 package com.luoluocaihong.springcloud.learn.servicederegister;
 
+import com.ecwid.consul.v1.ConsulClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.consul.discovery.ConsulDiscoveryProperties;
+import org.springframework.cloud.consul.serviceregistry.ConsulAutoRegistration;
+import org.springframework.context.ApplicationContext;
 
 /**
  * Created by xh on 2019/2/13.
@@ -10,7 +14,25 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ServiceApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        ApplicationContext ctx = SpringApplication.run(ServiceApplication.class, args);
+
+        //这样做可能此时ctx已经被关闭了。 因为Spring应用程序启动的时候已经注册了一个钩子(org.springframework.boot.SpringApplication.refreshContext中注册的,用于关闭应用上下文)
+        //改为在AutoDeregisterConfig中处理
+//        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                ConsulClient consulClient = ctx.getBean(ConsulClient.class);
+//                ConsulDiscoveryProperties properties = ctx.getBean(ConsulDiscoveryProperties.class);
+//                //获取当前微服务的实例ID
+//                String instanceId = ConsulAutoRegistration.getInstanceId(properties, ctx);
+//                //注销微服务实例
+//                consulClient.agentServiceDeregister(instanceId);
+//            }
+//        }));
+
+//        //for test
+//        Thread.sleep(10000);
+//        System.exit(1);
     }
 }
